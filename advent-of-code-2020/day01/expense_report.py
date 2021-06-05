c14eacc36cd5a641f150b1fd2acdeba07b67bb0e
Bessie Steinberg <5060285+BessieSteinberg@users.noreply.github.com>
2021-06-05 13:09:29 -0600

    day 1 star 2

--- advent-of-code-2020/day01/expense_report.py
+++ advent-of-code-2020/day01/expense_report.py
@@ -1,8 +1,8 @@
-def find_2020_sum(expenses):
+def find_sum_pair(expenses, sum_total=2020):
     expenses_set = set()
 
     for expense in expenses:
-        difference = 2020 - expense
+        difference = sum_total - expense
         if difference in expenses_set:
             return expense, difference
         else:
@@ -12,5 +12,21 @@ def find_2020_sum(expenses):
 
 
 def prod_of_2020_sum(expenses):
-    expense1, expense2 = find_2020_sum(expenses)
+    expense1, expense2 = find_sum_pair(expenses)
     return expense1 * expense2
+
+
+def prod_of_2020_3_val_sum(expenses):
+
+    for ii in range(len(expenses)):
+        expense1 = expenses[ii]
+        difference = 2020 - expense1
+
+        try:
+            expense2, expense3 = find_sum_pair(expenses[ii+1:], difference)
+            return expense1 * expense2 * expense3
+
+        except AttributeError:
+            pass
+
+    raise AttributeError("No three values sum to 2020")
